ca13863412bcd3f64668f3f796b4226bf0153948
MeshCollider <dobsonsa68@gmail.com>
2014-08-14 11:55:21 +1200

    Update CurrencyTextView.java

--- wallet/src/de/schildbach/wallet/ui/CurrencyTextView.java
+++ wallet/src/de/schildbach/wallet/ui/CurrencyTextView.java
@@ -33,7 +33,7 @@ import android.widget.TextView;
 import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.util.GenericUtils;
 import de.schildbach.wallet.util.WalletUtils;
-import net.meshcollision.omnicoin.wallet.R;
+import de.schildbach.wallet.omnicoin.R;
 
 /**
  * @author Andreas Schildbach
