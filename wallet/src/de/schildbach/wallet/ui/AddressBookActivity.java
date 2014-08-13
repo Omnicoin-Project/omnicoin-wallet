356285c3a53abb4930ac893904cc0085004695f2
MeshCollider <dobsonsa68@gmail.com>
2014-08-14 11:54:09 +1200

    Update AddressBookActivity.java

--- wallet/src/de/schildbach/wallet/ui/AddressBookActivity.java
+++ wallet/src/de/schildbach/wallet/ui/AddressBookActivity.java
@@ -38,7 +38,7 @@ import com.google.bitcoin.core.ECKey;
 
 import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.util.ViewPagerTabs;
-import net.meshcollision.omnicoin.wallet.R;
+import de.schildbach.wallet.omnicoin.R;
 
 
 /**
