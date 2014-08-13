ef423835bbec2bbe41278ccf5bfa432e3e77903a
MeshCollider <dobsonsa68@gmail.com>
2014-08-14 11:56:23 +1200

    Update FileAdapter.java

--- wallet/src/de/schildbach/wallet/ui/FileAdapter.java
+++ wallet/src/de/schildbach/wallet/ui/FileAdapter.java
@@ -28,7 +28,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import net.meshcollision.omnicoin.wallet.R;
+import de.schildbach.wallet.omnicoin.R;
 
 /**
  * @author Andreas Schildbach
