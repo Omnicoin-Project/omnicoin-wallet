641f325b9e568d47ffa8ce69850cfa2923641c30
MeshCollider <dobsonsa68@gmail.com>
2014-08-14 12:09:22 +1200

    Update WalletTransactionsFragment.java

--- wallet/src/de/schildbach/wallet/ui/WalletTransactionsFragment.java
+++ wallet/src/de/schildbach/wallet/ui/WalletTransactionsFragment.java
@@ -27,7 +27,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import de.schildbach.wallet.ui.TransactionsListFragment.Direction;
 import de.schildbach.wallet.util.ViewPagerTabs;
-import net.meshcollision.omnicoin.wallet.R;
+import de.schildbach.wallet.omnicoin.R;
 
 /**
  * @author Andreas Schildbach
