a24ae13051ce203ffdd874a007fefead1ba906b2
MeshCollider <dobsonsa68@gmail.com>
2014-08-14 12:06:58 +1200

    Update TransactionsListFragment.java

--- wallet/src/de/schildbach/wallet/ui/TransactionsListFragment.java
+++ wallet/src/de/schildbach/wallet/ui/TransactionsListFragment.java
@@ -79,7 +79,7 @@ import de.schildbach.wallet.util.Nfc;
 import de.schildbach.wallet.util.Qr;
 import de.schildbach.wallet.util.WalletUtils;
 
-import net.meshcollision.omnicoin.wallet.R;
+import de.schildbach.wallet.omnicoin.R;
 import de.schildbach.wallet.util.ThrottlingWalletChangeListener;
 
 
