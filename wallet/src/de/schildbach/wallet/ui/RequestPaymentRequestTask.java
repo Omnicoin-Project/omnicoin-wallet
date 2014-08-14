b2d8ec4a1fcc9d992bb4fe6496a90528ebd7931e
MeshCollider <dobsonsa68@gmail.com>
2014-08-14 12:04:08 +1200

    Update RequestPaymentRequestTask.java

--- wallet/src/de/schildbach/wallet/ui/RequestPaymentRequestTask.java
+++ wallet/src/de/schildbach/wallet/ui/RequestPaymentRequestTask.java
@@ -44,7 +44,7 @@ import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.PaymentIntent;
 import de.schildbach.wallet.util.Bluetooth;
 import de.schildbach.wallet.util.PaymentProtocol;
-import net.meshcollision.omnicoin.wallet.R;
+import de.schildbach.wallet.omnicoin.R;
 
 /**
  * @author Andreas Schildbach
