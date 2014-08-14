6020eb257703db27d4bbcc3b4717f9ee7060a088
MeshCollider <dobsonsa68@gmail.com>
2014-08-14 12:08:43 +1200

    Update WalletAddressesFragment.java

--- wallet/src/de/schildbach/wallet/ui/WalletAddressesFragment.java
+++ wallet/src/de/schildbach/wallet/ui/WalletAddressesFragment.java
@@ -59,7 +59,7 @@ import de.schildbach.wallet.util.BitmapFragment;
 import de.schildbach.wallet.util.Qr;
 import de.schildbach.wallet.util.WalletUtils;
 
-import net.meshcollision.omnicoin.wallet.R;
+import de.schildbach.wallet.omnicoin.R;
 
 
 /**
