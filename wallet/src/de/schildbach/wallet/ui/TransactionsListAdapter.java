679d79b08e790c95b07ee310136c23284bf1b3ab
MeshCollider <dobsonsa68@gmail.com>
2014-08-14 12:06:40 +1200

    Update TransactionsListAdapter.java

--- wallet/src/de/schildbach/wallet/ui/TransactionsListAdapter.java
+++ wallet/src/de/schildbach/wallet/ui/TransactionsListAdapter.java
@@ -51,7 +51,7 @@ import de.schildbach.wallet.AddressBookProvider;
 import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.util.CircularProgressView;
 import de.schildbach.wallet.util.WalletUtils;
-import net.meshcollision.omnicoin.wallet.R;
+import de.schildbach.wallet.omnicoin.R;
 
 /**
  * @author Andreas Schildbach
