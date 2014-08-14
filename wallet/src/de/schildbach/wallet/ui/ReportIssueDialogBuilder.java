d10649bb943d73167e507241e745ce3ff37f07c5
MeshCollider <dobsonsa68@gmail.com>
2014-08-14 12:01:47 +1200

    Update ReportIssueDialogBuilder.java

--- wallet/src/de/schildbach/wallet/ui/ReportIssueDialogBuilder.java
+++ wallet/src/de/schildbach/wallet/ui/ReportIssueDialogBuilder.java
@@ -46,7 +46,7 @@ import android.widget.Toast;
 import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.util.CrashReporter;
 import de.schildbach.wallet.util.Io;
-import net.meshcollision.omnicoin.wallet.R;
+import de.schildbach.wallet.omnicoin.R;
 
 /**
  * @author Andreas Schildbach
